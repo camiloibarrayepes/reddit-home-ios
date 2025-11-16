e5ebb9a0c5f9d9cee73b2a42008a2b7022b62555
camilo ibarra <camilo.ibarray@gmail.com>
2025-11-16 17:42:32 -0500

    Initial Commit

---
+++ reddit-home-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  reddit-home-ios
+//
+//  Created by Camilo Ibarra yepes on 16/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ reddit-home-iosTests/reddit_home_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  reddit_home_iosTests.swift
+//  reddit-home-iosTests
+//
+//  Created by Camilo Ibarra yepes on 16/11/25.
+//
+
+import Testing
+@testable import reddit_home_ios
+
+struct reddit_home_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
